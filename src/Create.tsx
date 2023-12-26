206854e6758d438e2b0b9c82084cc3dba68e314a
artog1809 <gdoq17@mail.ru>
2023-12-26 16:22:08 +0300

    add some styles for Create and Home component

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "./App.css"
 function Create() {
   return (
-    <div>
+    <div className = "create_form">
         <input type='text' name='' id='' />
         <button type='button'>Add</button>
     </div>
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Create from './Create'
+import "./App.css"
 
 function Home() {
-  return (
-    <div>
-        <h2>ToDo List</h2>
-        <Create />
-    </div>
-  )
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className='home'>
+            <h2 >ToDo List</h2>
+            <Create />
+            {todos.length === 0 ?
+                <div><h2>No Record</h2></div>
+                :
+                todos.map(todo => (
+                    <div>
+                        {todo}
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default Home
