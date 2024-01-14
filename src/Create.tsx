34efec977757a6e0399607bcfe80c6a3c4a8a551
artog1809 <gdoq17@mail.ru>
2024-01-14 16:58:46 +0300

    second commit

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css"
+import useInput from './hooks/useInput'
+import axios from 'axios'
+
 function Create() {
+  const task = useInput('');
+  
+  const handleAdd = () => {
+    axios.post('http://localhost:3001/add' , {task: task.value})
+    .then(result => location.reload())
+    .catch(err => console.log(err))
+  } 
+
   return (
     <div className = "create_form">
-        <input type='text' name='' id='' />
-        <button type='button'>Add</button>
+        <input type='text' placeholder='Entet text' {...task} />
+        <button type='button' onClick={handleAdd}>Add</button>
     </div>
   )
 }
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,49 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Create from './Create'
 import "./App.css"
+import axios from 'axios';
+import { BsCircleFill } from "react-icons/bs";
+import { BsTrashFill } from "react-icons/bs";
+import { BsCheckCircleFill } from 'react-icons/bs';
+
+// Определяем тип для объекта todo
+interface Todo {
+    _id: number; // Измените тип в соответствии с вашей структурой данных
+    task: string;
+    done: boolean;
+    // Добавьте другие свойства, если у ваших объектов todo есть дополнительные поля
+}
 
 function Home() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = () => {
+        axios.get('http://localhost:3001/get')
+            .then(result => setTodos(result.data))
+            .catch(err => console.log(err));
+    };
+
+    const handleEdit = (id: number) => {
+        axios.put(`http://localhost:3001/update/${id}`)
+            .then(result => {
+                fetchTodos();
+            })
+            .catch(err => console.log(err));
+    };
+
+    const handleDelete = (id: number) => {
+        axios.delete(`http://localhost:3001/delete/${id}`)
+            .then(result => {
+                fetchTodos();
+            })
+            .catch(err => console.log(err));
+    }
+
+
     return (
         <div className='home'>
             <h2 >ToDo List</h2>
@@ -12,8 +52,17 @@ function Home() {
                 <div><h2>No Record</h2></div>
                 :
                 todos.map(todo => (
-                    <div>
-                        {todo}
+                    <div className='task'>
+                        <div className='checkbox' onClick={() => handleEdit(todo._id)}>
+                            {todo.done ?
+                                <BsCheckCircleFill className='icon' />
+                                : <BsCircleFill className='icon' />
+                            }
+                            <p className={todo.done ? "line-through" : "linenot-through"}>{todo.task}</p>
+                        </div>
+                        <div>
+                            <span><BsTrashFill className='icon' onClick = {() => handleDelete(todo._id)}></BsTrashFill></span>
+                        </div>
                     </div>
                 ))
             }
