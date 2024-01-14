a26a71086ddb822b4d2e112ed5d1984f847e7999
artog1809 <gdoq17@mail.ru>
2024-01-14 18:54:28 +0300

    add placeholder for decription of task

--- src/Create.tsx
+++ src/Create.tsx
@@ -1,20 +1,21 @@
-import React, { useState } from 'react'
 import "./App.css"
 import useInput from './hooks/useInput'
 import axios from 'axios'
 
 function Create() {
-  const task = useInput('');
+  const taskName = useInput('');
+  const taskDescription = useInput('');
   
   const handleAdd = () => {
-    axios.post('http://localhost:3001/add' , {task: task.value})
-    .then(result => location.reload())
+    axios.post('http://localhost:3001/add' , {task: taskName.value, description: taskDescription.value})
+    .then(() => location.reload())
     .catch(err => console.log(err))
   } 
 
   return (
     <div className = "create_form">
-        <input type='text' placeholder='Entet text' {...task} />
+        <input type='text' placeholder='Write you want to do' {...taskName} />
+        <input type='text' placeholder="Write description of your task" {...taskDescription}/>
         <button type='button' onClick={handleAdd}>Add</button>
     </div>
   )
